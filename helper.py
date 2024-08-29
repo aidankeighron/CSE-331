25c313ace86fb3389c69255d973df25d67918e2c
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-08-29 15:37:37 -0400

    day 1 +

--- helper.py
+++ helper.py
@@ -1,26 +1,80 @@
 import time
 import numpy as np
+from tabulate import tabulate
 
-start_time = 0
-
-def iterations(count, output=False, input_generator=lambda: True):
+def time_test(default_input, input_generator=None, iteration_options=[1,100,1000,100_000]):
     def decorate(fn):
         def wrapper():
-            result = []
-            for _ in range(count):
-                result.append(fn(*input_generator()))
-            return result if output else True
+            rows = []
+            def run_iterations(n):
+                iterations = 0
+                elapsed_time = 0
+                for _ in range(n):
+                    start = time.time_ns()
+                    fn(input_generator() if input_generator else default_input)
+                    end = time.time_ns()
+                    iterations += 1
+                    elapsed_time += end-start
+                
+                rows.append([iterations, time_to_string(elapsed_time), time_to_string(elapsed_time/iterations)])
+                
+            for n in iteration_options:
+                run_iterations(n)
+                
+            print(tabulate(rows, headers=['Iterations', 'Total', 'Average'], tablefmt="plain"))
+            return True
         return wrapper
     return decorate
 
-def random_list_generator(n, max_int):
-    return lambda: [np.random.randint(max_int, size=n)]
+def iterations(count, input_generator=None):
+    def decorate(fn):
+        def wrapper():
+            elapsed_time = 0
+            iterations = 0
+            start_time = time.time_ns()
+            
+            if input_generator:
+                for _ in range(count):
+                    start = time.time_ns()
+                    fn(*input_generator())
+                    end = time.time_ns()
+                    iterations += 1
+                    elapsed_time += end-start
+            else:
+                for _ in range(count):
+                    start = time.time_ns()
+                    fn()
+                    iterations += 1
+                    end = time.time_ns()
+                    elapsed_time += end-start
+            
+            time_took = time.time_ns() - start_time
+            print(tabulate([[time_to_string(time_took), time_to_string(elapsed_time), time_to_string(elapsed_time/iterations)]], 
+                           headers=['Total', 'Elapsed', 'Average']))
+            return True
+        return wrapper
+    return decorate
 
-
-def start_timer():
-    global start_time
-    start_time = time.time_ns()
-
-def end_time():
-    time_took = (time.time_ns() - start_time) / 1e6
-    print("Program took", time_took, "ms to run.")
+def random_list_generator(max_size, max_int, random=False):
+    length = np.random.randint(max_size)
+    return lambda: [np.random.randint(max_int, size=length if random else max_size)]
+    
+def time_to_string(time):
+    units = "ns"
+    
+    microseconds = time > 1000
+    if microseconds:
+        units = 'μs'
+        time /= 1000
+    
+    milliseconds = time > 1000
+    if milliseconds:
+        units = 'ms'
+        time /= 1000
+    
+    seconds = time > 1000
+    if seconds:
+        units = 's'
+        time /= 1000
+    
+    return f"{round(time, 3)} {units}"
--- lectures/day1.py
+++ lectures/day1.py
@@ -1,10 +1,9 @@
 import sys
 sys.path.append("../CSE-331")
-from helper import start_timer, iterations, end_time, random_list_generator
+from helper import iterations, random_list_generator, time_test
 
-start_timer()
-
-@iterations(1_000, input_generator=random_list_generator(10, 100))
+@iterations(1000, input_generator=random_list_generator(100, 10000))
+# @time_test([1]*100)
 def run(l):
     if not len(l):
         l = [1,2,3]
@@ -17,5 +16,3 @@ def run(l):
         out.append(s/(i+1))
     # print(out)
 run()
-
-end_time()
